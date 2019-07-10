6cf988fe025f47dfa06b72671f9ee2b526cd0a4a
MasonCable <masoncablelsc@gmail.com>
2019-07-10 11:18:08 -0700

    react-app-heroku

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../App.css'
+
+const Header = () => {
+  return(
+    <div className='main'>
+      This is the header
+    </div>
+  )
+}
+export default Header
---
+++ src/components/main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import LandingPage from './landingpage.js'
+import AboutMe from './aboutme.js'
+import Contact from './contact.js'
+import Projects from './projects.js'
+import Resume from './resume.js'
+
+const changePath = () => {
+    
+}
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+)
+
+export default Main
